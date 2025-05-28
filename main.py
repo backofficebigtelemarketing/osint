7a84149300902333a290d5acced08d36706a24d3
backofficebigtelemarketing <backofficebigtelemarketing@gmail.com>
2025-05-29 00:29:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ def home(request: Request):
 def advanced_search(request: Request, input_data: str = Form(...)):
     links = [
         f"https://www.google.com/search?q={input_data}",
+        f"https://www.instagram.com/search?q={input_data}",
         f"https://www.bing.com/search?q={input_data}",
+        f"https://www.facebook.com/search?q={input_data}",
         f"https://yandex.com/search/?text={input_data}",
         f"https://www.linkedin.com/search/?text={input_data}",
         f"https://duckduckgo.com/?q={input_data}",
